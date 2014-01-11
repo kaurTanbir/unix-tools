ca5e03fef6c7139089dff85554304f0ddd01c3df
Tanbir <tanbirka@thoughtworks.com>
2014-01-11 14:43:10 +0530

    removed unnessary code

--- src/tanbirka/unixtools/api/WordCount.java
+++ src/tanbirka/unixtools/api/WordCount.java
@@ -3,19 +3,16 @@ package tanbirka.unixtools.api;
 
 public class WordCount{
 	public int lineCount(String text) {
-		int count = 0;
 		String[] lines = text.split("\n");
 		return lines.length;
 	}
 
 	public int wordCount(String text) {
-		int count = 0;
 		String[] words = text.split("\\s");
 		return words.length;
 	}
 
 	public int charCount(String text) {
-		int count = 0;
 		String[] characters = text.split("");
 		return characters.length;
 	}
