f79d2569db610b8133211bba27cb63f9ad1023c2
Tanbir <tanbirka@thoughtworks.com>
2014-01-14 18:05:50 +0530

    wc tested

---
+++ Test/WordCountTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import tanbirka.unixtools.api.WordCount;
+
+import static junit.framework.Assert.assertEquals;
+
+
+public class WordCountTest {
+    @Test
+    public void testLineCount() throws Exception {
+        WordCount count = new WordCount();
+        int noOfLines = count.wordCount("hello\n how\n are\n you");
+        assertEquals(4,noOfLines );
+    }
+
+    @Test
+    public void testWordCount() throws Exception {
+        WordCount count = new WordCount();
+        int noOfWords = count.wordCount("hello how are you");
+        assertEquals(4,noOfWords );
+    }
+
+    @Test
+    public void testCharCount() throws Exception {
+        WordCount count = new WordCount();
+        int noOfCharacters = count.charCount("hello how are you");
+        assertEquals(3, noOfCharacters);
+    }
+}
