4ead773839e4c8d61a54a3535f267408b83e9f6a
Tanbir <tanbirka@thoughtworks.com>
2014-01-14 22:30:48 +0530

    cut tested and working with only one field

---
+++ Test/tanbirka/unixtools/api/CutTest.java
@@ -0,0 +1,25 @@
+package tanbirka.unixtools.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CutTest {
+    @Test
+    public void testGetFieldData() throws Exception {
+        String input ="aaa,bbb\nccc,ddd";
+        String expected = "aaa\nccc\n";
+        Cut cut = new Cut();
+        String result = cut.getFieldData(input,1,",");
+        assertEquals(expected,result);
+    }
+    @Test
+    public void testGetFieldDataWhwnFieldNoMore() throws Exception {
+        String input ="aaa,bbb\nccc,ddd,eee";
+        String expected = "\neee\n";
+        Cut cut = new Cut();
+        String result = cut.getFieldData(input,3,",");
+        assertEquals(expected,result);
+    }
+}
