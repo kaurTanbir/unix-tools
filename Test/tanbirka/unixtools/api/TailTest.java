f35b3b09202c4401c9293007a669c81dfed92208
Tanbir <tanbirka@thoughtworks.com>
2014-01-18 13:10:05 +0530

    runTest working with build

--- Test/tanbirka/unixtools/api/TailTest.java
+++ Test/tanbirka/unixtools/api/TailTest.java
@@ -8,17 +8,17 @@ public class TailTest {
     @Test
     public void testGetLines() throws Exception {
         String data = "Kunal\nis\ngreat\nI\n";
-        String expected = "I\ngreat\nis\n";
+        String expected = "is\ngreat\nI\n";
         Tail ob = new Tail();
 
-        String actual = ob.getLines(data);
+        String actual = ob.getLines(data,3);
 
         assertEquals(expected, actual);
     }
     @Test
     public void testGet10Lines() throws Exception {
         String data = "Kunal\nis\ngreat\nI\nam\na\nbig\nfan\nof\nhis\ni\nthink\ntooo";
-        String expected = "tooo\nthink\ni\nhis\nof\nfan\nbig\na\nam\nI\n";
+        String expected = "I\nam\na\nbig\nfan\nof\nhis\ni\nthink\ntooo\n";
         Tail ob = new Tail();
 
         String actual = ob.getLines(data);
